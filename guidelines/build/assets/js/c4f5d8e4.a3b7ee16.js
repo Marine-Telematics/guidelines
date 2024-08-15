3126b1552763c485a24adacaed378bf72e9f1863
gab5987 <biell.gb4@gmail.com>
2024-08-15 15:03:56 -0300

    added build files

---
+++ guidelines/build/assets/js/c4f5d8e4.a3b7ee16.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkguidelines=self.webpackChunkguidelines||[]).push([[634],{1459:(e,i,s)=>{s.r(i),s.d(i,{default:()=>a});s(8774);var n=s(4586),t=s(781);s(1107);var u=s(4848);function a(){const{siteConfig:e}=(0,n.A)();return(0,u.jsx)(t.A,{title:"Marine Telematics docs",children:(0,u.jsx)("main",{})})}}}]);
